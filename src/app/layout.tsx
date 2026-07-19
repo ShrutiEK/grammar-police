1de080e4c0eb92753c3a219e3ed18a97bedc6c5f
Harshna N Haswani <hharshna.n@gmail.com>
2026-07-19 19:38:26 +0530

    chore: add hotjar tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {
   Atkinson_Hyperlegible_Next,
 } from "next/font/google";
+import Script from "next/script";
 import type { ReactNode } from "react";
 
 import { AccessibilityToolkit } from "@/features/accessibility/accessibility-toolkit";
@@ -30,6 +31,16 @@ export default function RootLayout({ children }: RootLayoutProperties) {
       <body>
         {children}
         <AccessibilityToolkit />
+        <Script id="hotjar" strategy="afterInteractive">
+          {`(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6749583,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </Script>
       </body>
     </html>
   );
